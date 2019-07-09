e692d2b380314a0858eb96b938b26c9bd1127af0
zhuhaizhong <zhuhaizhong@zhonghaizhongdeMac-mini.local>
2019-07-09 16:50:03 +0800

    test

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php
+//    $txtfile = 'test';
+//
+//    $file = fopen($txtfile,'r');
+//
+////$i = 0;
+//while (!feof($file)){
+//    $tmp = fgets($file);
+//
+////    $tmp = preg_replace("/[A-Z]*|_|=|;|\/|[0-9]*/","",$tmp);
+////    $tmp = preg_replace("/[^0-9]*/","",$tmp);
+//    $tmp = preg_replace("/const*/","",$tmp);
+//    echo $tmp;
+//    echo '<br>';
+//}
+//fclose($file);
+/*
+$formation_ids = '1:;2:;3:';
+$stage_id = 2;
+$hero_ids = '3124,3455,1224';
 
-    phpinfo();
+$formation_ids = explode(';', $formation_ids);
+
+var_dump($formation_ids);
+
+foreach ($formation_ids as $key => $value){
+    if (($stage_id - 1) == $key){
+        $tmp = explode(':', $value);
+        $tmp[1] = $hero_ids;
+        $tmp = implode(':', $tmp);
+
+        $formation_ids[$key] = $tmp;
+    }
+}
+
+var_dump(implode(';', $formation_ids));*/
+
+$test = '1234,1234124,123123';
+
+$a = explode(';', $test);
+
+var_dump(count($a) <= 1);
+
+
+
